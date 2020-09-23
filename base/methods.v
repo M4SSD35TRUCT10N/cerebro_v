4d4ba7118e1e23a466ab992c8a7e6befd3f50e23
Enrico Lefass <M4SSD35TRUCT10N@users.noreply.github.com>
2020-09-23 13:20:16 +0200

    updating file via d&d upload
    * adds preview of documentation to functions
    * implements function get_neuron_by_string

--- base/methods.v
+++ base/methods.v
@@ -16,8 +16,12 @@
 
 module base
 
+// This module provides the base functionality of cerebro.
+
+import regex
 import time
 
+// get_new_neuron_id returns a new unique id for the given Cerebro as u64.
 fn get_new_neuron_id(c Cerebro) u64 {
   mut id := u64(0)
 
@@ -30,7 +34,8 @@ fn get_new_neuron_id(c Cerebro) u64 {
   return id + 1
 }
 
-pub fn (mut c Cerebro) create_neuron(n_name, n_icon string, n_depth u64, n_desc, n_src, n_type_id string, n_content Data, n_links []Link) {
+// create_neuron creates a new Neuron within the given Cerebro.
+pub fn (mut c Cerebro) create_neuron(n_name, n_icon string, n_depth u64, n_desc, n_src string, n_type_id TypeID, n_content Data, n_links []Link) {
   n_time := time.now().get_fmt_str(.dot, .hhmmss24, .ddmmyyyy)
 
   c.neurons << Neuron {
@@ -50,9 +55,12 @@ pub fn (mut c Cerebro) create_neuron(n_name, n_icon string, n_depth u64, n_desc,
   }
 }
 
-pub fn (mut c Cerebro) forget_neuron(n_id u64) {
+// forget_neuron marks the Neuron with the given id (as u64) as deceased and
+// writes the current time when it forgot the Neuron in the fields modified
+// and visited.
+pub fn (mut c Cerebro) forget_neuron(id u64) {
   for i, n in c.neurons {
-    if n.id == n_id {
+    if n.id == id {
       n_time := time.now().get_fmt_str(.dot, .hhmmss24, .ddmmyyyy)
       
       c.neurons[i].deceased = true
@@ -62,6 +70,8 @@ pub fn (mut c Cerebro) forget_neuron(n_id u64) {
   }
 }
 
+// get_neuron_by_id returns the Neuron with the given id (as u64) as Option
+// type.
 pub fn (c Cerebro) get_neuron_by_id(id u64) ?Neuron {
   for n in c.neurons {
     if n.id == id {
@@ -71,17 +81,71 @@ pub fn (c Cerebro) get_neuron_by_id(id u64) ?Neuron {
   return error('A neuron with id $id was not found!')
 }
 
-pub fn (c Cerebro) get_neuron_by_name(name string) ?[]Neuron {
+// get_neuron_by_type_id returns an array of Neurons which matches the given
+// TypeID as an Option type.
+pub fn (c Cerebro) get_neuron_by_type_id(type_id TypeID) ?[]Neuron {
   mut neurons := []Neuron{}
 
   for n in c.neurons {
-    if n.name == name {
+    if n.type_id == type_id {
       neurons << n
     }
   }
 
   if neurons.len == 0 {
-    return error('A neuron with the name $name was not found!')
+    return error('A neuron with the type_id $type_id was not found!')
+  } else {
+    return neurons
+  }
+}
+
+// get_neuron_by_string returns an array of Neurons which matches the given
+// string (with additional options) as an Option type.
+pub fn (c Cerebro) get_neuron_by_string(str string, cs, bn, is_rx_str bool) ?[]Neuron {
+  mut neurons     := []Neuron{}
+  mut rx_pattern  := ''
+  mut cmp_str     := ''
+
+  if is_rx_str {
+    rx_pattern = str
+  }
+
+  if !is_rx_str && cs {
+    rx_pattern = '(' + str + ')'
+  }
+
+  if !is_rx_str && !cs {
+    rx_pattern = '(' + str.to_lower() + ')'
+  }
+
+  mut rx := regex.regex_opt(rx_pattern)?
+
+  for n in c.neurons {
+    if bn && cs {
+      cmp_str = n.name
+    }
+
+    if bn && !cs {
+      cmp_str = n.name.to_lower()
+    }
+
+    if !bn && cs {
+      cmp_str = n.name + '|' + n.description
+    }
+
+    if !bn && !cs {
+      cmp_str = n.name.to_lower() + '|' + n.description.to_lower()
+    }
+
+    start, end := rx.find(cmp_str)
+
+    if start >= 0 && end > 0 {
+      neurons << n
+    }
+  }
+
+  if neurons.len == 0 {
+    return error('A neuron with the name $str was not found!')
   } else {
     return neurons
   }
